893047444efaa0362cd133d8309f8d8e21046a2e
smenarru <smenarru@gmail.com>
2014-01-09 23:41:26 -0800

    preliminary design. back to db

--- rubytest.rb
+++ rubytest.rb
@@ -13,7 +13,13 @@ File.open("IMPORT_READY.txt", "r").each_line do |line|
 		end 
 		@g=Team.find_by(name: @a[1]).id
 		@h=Team.find_by(name: @a[4]).id
-		f=Game.new(id: @counter,home_team_id: @g, away_team_id: @h, home_score: @a[2], away_score: @a[3], played_at: @a[5])
+		if @a[5].respond_to?(:split)
+			@j=@a[5].split(",")
+		else
+			@j=[1,1,14]
+		end
+		t=Date.new(@j[0].to_i,@j[1].to_i,@j[2].to_i)
+		f=Game.new(id: @counter,home_team_id: @g, away_team_id: @h, home_score: @a[2], away_score: @a[3], played_at: t)
 		f.save
 	end
 end
