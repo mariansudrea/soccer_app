8c0532c574c9651b77267e7812c535087c68757b
smenarru <smenarru@gmail.com>
2014-01-12 19:20:06 -0800

    3

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,5 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-	config.assets.precomplie += [
-		custom-big.css.scss
-	]
   end
 end
