49fee3c2b67f0f849b16405cb3e0c0e626ab010a
smenarru <smenarru@gmail.com>
2014-01-30 17:38:09 -0800

    first_ajax_complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,10 @@ class GamesController < ApplicationController
 	def view
 		@game = Game.find(params[:id])
 		@stats = @game.stats.all.collect.sort_by { |x| [x.goals,x.assists] }.reverse
+		respond_to do |format| 
+			format.html
+			format.js
+		end
 	end
 
 end
