6ba61f544441e91d9689914ff7e00ed7c96e91bd
smenarru <smenarru@gmail.com>
2014-02-02 23:07:10 -0800

    player_nav

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,14 @@ class PlayersController < ApplicationController
 
 	def view
 		@player = User.find(params[:id])
+		@gpg = 0
+		@apg = 0
+		@player.stats.each do |stat|
+			@gpg = stat.goals.to_i + @gpg||0.00
+			@apg = stat.assists.to_i + @apg||0.00
+		end
+		@gpg = (@gpg.to_f / @player.stats.length).round(2)
+		@apg = (@apg.to_f / @player.stats.length).round(2)
 	end
 
 end
