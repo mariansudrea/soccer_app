4ffd55381bb60a73c13e727fc099c45c5ac1a3d7
Marian Udrea <smenarru@gmail.com>
2015-11-18 09:57:37 -0800

    some_css_updates_2015_11_18

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
 	def view
 		@game = Game.find(params[:id])
+		@sloboz = params[:sloboz]
 		@stats = @game.stats.all.collect.sort_by { |x| [x.goals,x.assists] }.reverse
 		respond_to do |format| 
 			format.html
