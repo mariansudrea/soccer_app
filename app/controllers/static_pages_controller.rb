869412c1cb130d12a523fe449c599db4b5de7371
Marian Udrea <smenarru@gmail.com>
2016-11-11 14:34:53 -0800

    new_season_modifications

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -122,7 +122,8 @@ class StaticPagesController < ApplicationController
 
 
 	@season_start = Array.new()
-	@season_start[133]=1667;
+	@season_start[134]=9943;
+	@season_start[133]=943;
 	@season_start[132]=911;
 	@season_start[131]=883;
 	@season_start[130]=859;
